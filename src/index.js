41208459f1ff1c6c4851a40c7f4e40453cb422b9
Javier García Latorre <garcialatorre365@gmail.com>
2022-07-11 20:48:37 +0200

    feat(game): add stepnumber to state

--- src/index.js
+++ src/index.js
@@ -54,6 +54,7 @@ function Square(props) {
             history: [{
                 squares: Array(9).fill(null),
             }],
+            stepNumber: 0,
             xIsNext: true,
         }
     }
